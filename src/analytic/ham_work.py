5d257c9e55ae7cd0d27ce2cd57409b805ca75015
wolfram74 <phaugen@gmail.com>
2019-07-16 09:00:19 -0500

    a bit more printing

--- src/analytic/ham_work.py
+++ src/analytic/ham_work.py
@@ -31,11 +31,14 @@ def circular_orbit():
     dot_tht = new_Ham.diff(new_momenta[2])
     stability1 = dot_phit-2*dot_tht
     sympy.pprint(stability1)
+    mathjaxify(stability1)
     p_phit_soltn = sympy.solve(stability1, new_momenta[1])[0]
     sympy.pprint(p_phit_soltn)
+    mathjaxify(p_phit_soltn)
     #Lt = ptht+2pt
     Lt = new_momenta[2]+2*p_phit_soltn
     sympy.pprint(Lt)
+    mathjaxify(Lt)
     #dot(pr)=0
     r_force = -new_Ham.diff(new_positions[3])
     r_force = r_force.subs([
@@ -43,11 +46,13 @@ def circular_orbit():
         (new_positions[1]-2*new_positions[2],0)
         ])
     sympy.pprint(r_force)
+    mathjaxify(r_force)
     p_tht_soltn = sympy.solve(r_force, new_momenta[2])[1].simplify()
     sympy.pprint(p_tht_soltn)
     sympy.pprint(Lt.subs(new_momenta[2], p_tht_soltn))
     p_phit_soltn_inr = p_phit_soltn.subs(new_momenta[2], p_tht_soltn)
-
+    sympy.pprint(p_phit_soltn_inr)
+    mathjaxify(p_phit_soltn_inr)
     circ_energy = new_Ham.subs([
         (new_positions[0],0),
         (new_positions[1],0),
@@ -59,6 +64,7 @@ def circular_orbit():
         ])
     r = new_positions[3]
     sympy.pprint(circ_energy)
+    mathjaxify(circ_energy)
     sympy.pprint(p_tht_soltn.subs(r, 1.5).evalf())
     sympy.pprint(p_phit_soltn_inr.subs(r, 1.5).evalf())
 
